c3c36b24e5e967e08e4f26cdbb4c7bbf793a5d9b
Mitch Kennedy <mitchk@runbox.com>
2025-08-07 10:02:03 +1000

    work

--- log_server/src/main.zig
+++ log_server/src/main.zig
@@ -1,26 +1,13 @@
 const std = @import("std");
 const log_server = @import("log_server");
+const Config = @import("Config.zig");
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try log_server.bufferedPrint();
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    var gpa = std.heap.DebugAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+    const file = try std.fs.cwd().readFileAlloc(alloc, "config.json", 100_000);
+    defer alloc.free(file);
+    const config = try std.json.parseFromSlice(Config.Config, alloc, file, .{});
+    defer config.deinit();
 }
