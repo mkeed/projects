199fdc06af8cabc278c5fcf8190f5cbf6759d616
Mitch Kennedy <mitchk@runbox.com>
2025-10-28 20:36:57 +1100

    work

--- editor_daemon/libs/fonts/src/tables/GSUB.zig
+++ editor_daemon/libs/fonts/src/tables/GSUB.zig
@@ -3,6 +3,14 @@ const util = @import("../utils.zig");
 
 pub const GSUB = struct {};
 
+const GSUB_header = struct {
+    majorVersion: u16,
+    minorVersion: u16,
+    scriptListOffset: u16,
+    featureListOffset: u16,
+    lookupListOffset: u16,
+};
+
 pub fn decode(data: []const u8) !void {
     //
 }
